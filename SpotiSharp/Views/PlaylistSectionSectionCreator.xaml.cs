81f8f196624e303173b0d2c07f73e59170830bc4
Reece Shandro <reeceshandro@outlook.com>
2025-08-22 22:11:50 +1000

    Add control for multi select drop down/add multi song selection to playlist

--- SpotiSharp/Views/PlaylistSectionSectionCreator.xaml.cs
+++ SpotiSharp/Views/PlaylistSectionSectionCreator.xaml.cs
@@ -1,3 +1,5 @@
+using SpotiSharp.Enums;
+using SpotiSharp.Models;
 using SpotiSharp.ViewModels;
 
 namespace SpotiSharp.Views;
@@ -8,5 +10,41 @@ public partial class PlaylistSectionSectionCreator : ContentView
 	{
 		InitializeComponent();
         BindingContext = new PlaylistSectionSectionCreatorViewModel();
+    }
+
+    private void SectionTypeChangedHandler(object sender, EventArgs e)
+    {
+        var picker = (Picker)sender;
+        var sectionType = (PlaylistSectionType)picker.SelectedItem;
+
+        lblPlaceholder.IsVisible = false;
+        entNumericValue.IsVisible = false;
+        msSongs.IsVisible = false;
+        switch (sectionType.SectionType)
+        {
+
+            case PlaylistSectionEnum.EntirePlaylist:
+                break;
+            case PlaylistSectionEnum.PercentageOfNewPlaylistRandom:
+                entNumericValue.IsVisible = true;
+                break;
+            case PlaylistSectionEnum.FixedAmountSelected:
+                msSongs.IsVisible = true;
+                break;
+            case PlaylistSectionEnum.FixedAmountRandom:
+                entNumericValue.IsVisible = true;
+                break;
+        }
+    }
+
+    public void Playlist_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        var picker = (Picker)sender;
+        var selectedItem = (Playlist)picker.SelectedItem;
+
+        if (BindingContext is PlaylistSectionSectionCreatorViewModel vm)
+        {
+            vm.OnSelectedPlaylistChanged(selectedItem);
+        }
     }
 }
