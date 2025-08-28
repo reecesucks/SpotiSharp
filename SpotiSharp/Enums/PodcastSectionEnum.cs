dbe39c72754edbc26d74f6c1265a2311e68365f6
Reece Shandro <reeceshandro@outlook.com>
2025-08-28 20:11:57 +1000

    Add podcast selection section/load/seclect episode

---
+++ SpotiSharp/Enums/PodcastSectionEnum.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace SpotiSharp.Enums
+{
+    public  enum PodcastSectionEnum
+    {
+        [Description("Newest Unplayed")]
+        NewestUnplayed,
+        [Description("Random Unplayed")]
+        RandomUnplayed,
+        [Description("Select Episodes")]
+        SelectEpisodes,
+    }
+}
---
+++ SpotiSharp/Models/PodcastSectionType.cs
@@ -0,0 +1,16 @@
+﻿using SpotiSharp.Enums;
+
+namespace SpotiSharp.Models
+{
+    public class PodcastSectionType
+    {
+        public PodcastSectionEnum SectionType { get; set; }
+        public string SectionTypeDescription { get; set; }
+
+        public PodcastSectionType(PodcastSectionEnum sectionType, string description)
+        {
+            SectionType = sectionType;
+            SectionTypeDescription = description;
+        }
+    }
+}
